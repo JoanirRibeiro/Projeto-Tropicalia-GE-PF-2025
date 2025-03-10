c4ca57aa1d94204bc0f702371839e6801fe7a051
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2025-03-10 17:39:45 +0000

    linkando o arquivo JS ao HTML

--- script.js
+++ script.js
@@ -8,5 +8,10 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
 
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+    })
+
 }
 )
