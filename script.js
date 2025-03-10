132518adea8ed45cdd383a836155c3090523e16c
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2025-03-10 17:52:21 +0000

    .

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener ('DOMContentLoaded', function () {
     let tamanhoAtualFonte = 1;
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
 
-    aumentaFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte += 0.1;
+    aumentaFonteBotao.addEventListener ('click', function () {
+        tamanhoAtualFonte += 0.2;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
 
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
     diminuiFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte -= 0.1;
+        tamanhoAtualFonte -= 0.2;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
 
