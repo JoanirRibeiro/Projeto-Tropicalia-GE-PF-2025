c2c238090b684736fa5f4a57e4235a32fc31d9be
JOANIR FERNANDO RIBEIRO <108032158+JoanirRibeiro@users.noreply.github.com>
2025-03-24 17:37:17 +0000

    .

--- script.js
+++ script.js
@@ -7,19 +7,22 @@ document.addEventListener('DOMContentLoaded', function () {
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
     })
-  
+
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
-
-    aumentaFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte += 0.2;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
-    })
-
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
-    diminuiFonteBotao.addEventListener('click', function () {
-        tamanhoAtualFonte -= 0.2;
+    let tamanhoAtualFonte = 1;
+    
+    aumentaFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
+    diminuiFonteBotao.addEventListener('click', function () {
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
     })
 
 }
